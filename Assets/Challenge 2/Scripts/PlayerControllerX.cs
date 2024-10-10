6ee7c7866494c5cb3f74d5eb21491d6f9d101d00
Githiora <gabriel.wamunyu@iconicmoments.co>
2024-10-10 19:14:26 +0300

    prevent user from spamming spawning. Allow spawning after 1 second has passed since last spawn

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,19 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    private float timeInterval = 1.0f;
+    private float prevTime;
+
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && (prevTime == 0 || ((Time.time - prevTime) > timeInterval)))
         {
+
+            prevTime = Time.time;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
         }
     }
 }
